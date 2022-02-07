7a72025364ae8117352af078d83e769c5c2a8368
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-02-07 18:22:53 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const messages = ['mess1', 'mess2', 'mess3'];
+
+function writeCards(names, occasion) {
+    for (let i = 0; i < messages.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
+        }
+    return messages;
+}
+
+function countDown() {
+    let down = 10;
+    while (down > -1) {
+        console.log(down--);
+    }
+}
+
